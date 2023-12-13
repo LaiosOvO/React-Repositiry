c53325c9abcea90730456462602624115acf1b40
laios <1624456575@qq.com>
2023-12-13 17:22:41 +0800

    useState() - basic demo - { object,array,object+array } update data

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # React-Repositiry
 学习react的汇总仓库
+
+
+
+# 企业级应用
+
+https://juejin.cn/post/6885957893724569613
---
+++ hello-world/demo-syntax/src/components/test/syntax/EffectDemo.jsx
@@ -0,0 +1,16 @@
+import {useEffect} from 'react';
+
+function EffectDemo(){
+
+
+
+    return (
+        <>
+
+        </>
+    )
+
+}
+
+
+export default EffectDemo;
